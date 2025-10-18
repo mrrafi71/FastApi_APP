62c244f735c3a479815ef665b928e65bcb886648
MR Rafi <mdsaiful9095133@gmail.com>
2025-10-18 18:30:25 +0600

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import json
+
+app = FastAPI()
+
+frontendUrl = os.getenv("FRONTEND_URL", "*")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontendUrl],  # use your domain in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def Home():
+    return {"massage": "Hello Saiful Islam Rafi!"}
+
+def projects():
+    with open('projects.json', 'r') as f:
+        return json.load(f)
+
+@app.get("/projects")
+def get_projects(): 
+    return projects()
+
+
+def achivements():
+    with open('achivement.json', 'r') as f:
+        return json.load(f)
+
+@app.get("/achivements")
+def get_achivements(): 
+    return achivements()
